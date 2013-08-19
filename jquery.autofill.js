c2e7562198d4095d7c584fca75f4efff2219d6ee
root <root@v2inmumpc510>
2013-08-19 13:57:58 +0530

    1.0.0

---
+++ jquery.autofill.js
@@ -0,0 +1,66 @@
+/*
+
+* Author - Harshen Amarnath Pandey
+* Version - 1.0.0
+* Release - 16th August 2013
+* Copyright (c) 2013 - 2016 Harshen Pandey
+*/
+
+ (function( $ ) {
+
+    $.fn.autofill = function( options ) {
+        $.fn.autofill.defaults.fieldId = this.attr('id') + "_copy";
+        return this.each( function() {
+            autofillboxes( $(this), options );
+        });
+    };
+
+    function autofillboxes( $this , options ) {
+            var opts = $.extend( {}, $.fn.autofill.defaults, options );
+            var $this = $this;
+            $this.blur( function() {
+                
+                if( options.fieldId == undefined && options.overrideFieldEverytime == undefined ) {
+                    if( $( "#" + opts.fieldId ).val() == "" ) {
+                        $( "#" + opts.fieldId ).val( $this.val() );
+                        $this.unbind('blur');
+                    }
+                }
+
+                else if( options.fieldId == undefined && options.overrideFieldEverytime == false ) {
+                    if( $( "#" + opts.fieldId ).val() == "" ) {
+                        $( "#" + opts.fieldId ).val( $this.val() );
+                        $this.unbind('blur');
+                    }
+                }
+
+                else if( options.fieldId == undefined && options.overrideFieldEverytime == true ) {
+                        $( "#" + opts.fieldId ).val( $this.val() );
+                }
+                
+                else if( options.fieldId != undefined && options.overrideFieldEverytime == undefined ) {
+                    if( $( "#" + opts.fieldId ).val() == "" ) {
+                        $( "#" + opts.fieldId ).val( $this.val() );
+                        $this.unbind('blur');
+                    }
+                }
+
+                else if( options.fieldId != undefined && options.overrideFieldEverytime == true ){
+                    $( "#" + opts.fieldId ).val( $this.val() );
+                }
+
+                else if( options.fieldId != undefined && options.overrideFieldEverytime == false ){
+                    if( $( "#" + opts.fieldId ).val() == "" ) {
+                        $( "#" + opts.fieldId ).val( $this.val() );
+                        $this.unbind('blur');
+                    }
+                }
+
+            });
+    };
+    
+    $.fn.autofill.defaults = {
+        overrideFieldEverytime : false
+    };
+
+ }(jQuery));
